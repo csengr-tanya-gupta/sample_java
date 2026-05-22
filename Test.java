605c399d3da3061a0ba429c002118602fec0bd7d
Tanya Gupta <csengr.tanya.gupta@gmail.com>
2026-05-22 14:29:51 +0530

    updateed

--- Test.java
+++ Test.java
@@ -7,6 +7,7 @@ public class Test {
     int result = a*b;
     System.out.println("The sum is " +result);
     System.out.println("Tanya Gupta");
+    System.out.println("with ngrok");
     
   }
   public static void main(String[] args)
